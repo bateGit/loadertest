c19dd8b49ca2204883b608c985da4f60defcd179
deqingyan <deqingyan@tencent.com>
2019-07-29 16:17:18 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,9 @@
+const getOptions = require('loader-utils')
+
+module.exports = function(source) {
+    const options = getOptions.getOptions(this);
+
+    source = source.replace(/\[name\]/g, options.name);
+
+    return `export default ${ JSON.stringify(source)}`;
+}
